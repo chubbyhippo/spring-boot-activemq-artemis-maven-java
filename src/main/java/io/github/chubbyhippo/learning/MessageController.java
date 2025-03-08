c7bacc80e9bc27e5c832294aeee6e96f8c6b9a15
ch <mkmsacc@outlook.com>
2025-03-08 21:47:13 +0700

    Fix HTTP method for /receive endpoint to use GET.
    Updated the /receive endpoint to use @GetMapping instead of @PostMapping, aligning with RESTful conventions. This ensures better semantic meaning, as the endpoint only retrieves data without modifying any state.

--- src/main/java/io/github/chubbyhippo/learning/MessageController.java
+++ src/main/java/io/github/chubbyhippo/learning/MessageController.java
@@ -1,5 +1,6 @@
 package io.github.chubbyhippo.learning;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class MessageController {
         messageSenderService.send(message);
     }
 
-    @PostMapping("/receive")
+    @GetMapping("/receive")
     List<String> receive() {
         return messageListenerService.getReceivedMessages();
     }

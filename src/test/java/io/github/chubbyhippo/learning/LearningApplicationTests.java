f5c8f4ca4ff9e075244a82e1494473ba55823afd
ch <mkmsacc@outlook.com>
2025-03-08 21:03:59 +0700

    Add JMS message send/receive test with MockMvc
    Introduced a new test to verify sending and receiving JMS messages using MockMvcTester. Configured Awaitility to ensure messages are received within 5 seconds. Added necessary annotations and dependencies for testing in a web environment.

--- src/test/java/io/github/chubbyhippo/learning/LearningApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/learning/LearningApplicationTests.java
@@ -1,17 +1,43 @@
 package io.github.chubbyhippo.learning;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
 
 @Import(TestcontainersConfiguration.class)
 @Testcontainers(disabledWithoutDocker = true)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class LearningApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvcTester mockMvcTester;
+    @Autowired
+    private MessageListenerService messageListenerService;
+
+    @Test
+    @DisplayName("should send and receive jms message")
+    void shouldSendAndReceiveJmsMessage() {
+        mockMvcTester.post()
+                .uri("/send")
+                .content("hello")
+                .exchange();
+
+        await().atMost(5, TimeUnit.SECONDS)
+                .until(() -> {
+                    System.out.println(messageListenerService.getReceivedMessages());
+                    return messageListenerService.getReceivedMessages()
+                            .contains("hello");
+                });
+    }
 
 }

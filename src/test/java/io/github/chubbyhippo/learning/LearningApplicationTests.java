35fd0fb929f09b6c92f36aa0408d63e4c80b2d5d
ch <mkmsacc@outlook.com>
2025-03-08 15:29:33 +0700

    Add Testcontainers support to integration tests
    Enabled Testcontainers for integration tests by adding the `@Testcontainers` annotation. This ensures tests requiring Docker will only run if Docker is available, improving test reliability and flexibility.

--- src/test/java/io/github/chubbyhippo/learning/LearningApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/learning/LearningApplicationTests.java
@@ -3,8 +3,10 @@ package io.github.chubbyhippo.learning;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Import(TestcontainersConfiguration.class)
+@Testcontainers(disabledWithoutDocker = true)
 @SpringBootTest
 class LearningApplicationTests {
 
